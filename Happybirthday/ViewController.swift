e4e254f5fd593998ff04dc9e174c777177d30ea2
Maho Misumi <maho.smile606@gmail.com>
2017-10-22 22:22:33 +0900

    なんで＋押すと、相手の数字が移ってしまうのか

--- Happybirthday/ViewController.swift
+++ Happybirthday/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var number: Int = 0
+    @IBOutlet var monthlabel: UILabel!
+    @IBOutlet var daylabel: UILabel!
+    @IBOutlet var textlabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func monthplus(){
+        number = number + 1
+        monthlabel.text = String(number)
+         if monthlabel.text == "6" && daylabel.text == "6" {
+            textlabel.text = "お誕生日おめでとう！"
+        } else {
+            textlabel.text = " "
+        }
+    }
+    @IBAction func monthminus(){
+        number = number - 1
+        monthlabel.text = String(number)
+        if monthlabel.text == "6" && daylabel.text == "6" {
+            textlabel.text = "お誕生日おめでとう！"
+        } else {
+    textlabel.text = " "
+    }
+    }
+    @IBAction func dayplus(){
+        number = number + 1
+        daylabel.text = String(number)
+        if monthlabel.text == "6" && daylabel.text == "6" {
+            textlabel.text = "お誕生日おめでとう！"
+        } else {
+             textlabel.text = " "
+        }
+    }
+    @IBAction func dayminus(){
+        number = number - 1
+        daylabel.text = String(number)
+        if monthlabel.text == "6" && daylabel.text == "6" {
+            textlabel.text = "お誕生日おめでとう！"
+        } else {
+            textlabel.text = " "
+        }
+    }
 }
 
